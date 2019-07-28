219842c075d6bc06a5760be9c184c3e08d892837
Roei Bracha <roei635@gmail.com>
2019-07-28 09:16:06 +0300

    styling

---
+++ src/pages/homepage/HomePage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../commponents/directory/Directory.component';
+const HomePage = ()=>{
+    return(
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
+
+export default HomePage
