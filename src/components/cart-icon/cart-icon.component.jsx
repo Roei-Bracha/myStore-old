503a3af140b5f550e712af4112edc1cd63d573ed
roei.bracha@otorio.io <roei635@gmail.com>
2020-03-14 22:16:07 +0200

    create the cart item for the drop down

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,19 +1,25 @@
+
 import React from 'react';
-import {connect} from 'react-redux'
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import { connect } from 'react-redux';
 
-import './cart-icon.styles.scss'
-import {toggleCartHidden} from "../../redux/cart/cart.actions";
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartIcon = ({toggleCartHidden})=>(
-  <div className={'cart-icon'} onClick={()=>{toggleCartHidden()}}>
-    <ShoppingIcon className={'shopping-icon'}/>
-    <span className={'item-count'}>0</span>
-  </div>
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = ({ toggleCartHidden }) => (
+    <div className='cart-icon' onClick={toggleCartHidden}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>0</span>
+    </div>
 );
 
-const mapDispatchToProps = (dispatch)=>({
-  toggleCartHidden: ()=>dispatch(toggleCartHidden())
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null,mapDispatchToProps) (CartIcon);
+export default connect(
+    null,
+    mapDispatchToProps
+)(CartIcon);
